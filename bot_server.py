83ba7bf3e1386d74a0838d7a8998bd818b195c8c
Kimberly McCarty <mccartyt1@montclair.edu>
2017-09-18 00:47:44 -0400

    Added basic queue functionality to flask without celery!

--- bot_server.py
+++ bot_server.py
@@ -11,22 +11,28 @@ from flask import Flask, request, url_for, jsonify
 from celery import Celery
 from celery.bin import worker
 import quote_bot
+import queue_bot
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+#app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+#app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+#celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+#celery.conf.update(app.config)
 
-worker = worker.worker(app=celery)
-options =  {'concurrency': 1, 'broker':app.config['CELERY_BROKER_URL']}
+#worker = worker.worker(app=celery)
+#options =  {'concurrency': 1, 'broker':app.config['CELERY_BROKER_URL']}
 
+"""
 @celery.task(bind=True)
 def queue_search(self, query, author):
     self.update_state(state="SEARCHING")
     gr = quote_bot.GoodReads()
     return gr.search(query, author)
+    """
+
+queue_bot.start_worker()
+
 
 @app.route('/search_quote', methods=['POST'])
 def search_quote():
@@ -34,24 +40,23 @@ def search_quote():
     try:
         query = request.args['query']
         author = request.args['author']
-        quote_task = queue_search.delay(query, author)
+        #quote_task = queue_search.delay(query, author)
+        quote_task = queue_bot.add_to_queue(query, author)
     except IndexError:
         error = "Invalid parameters, /search_quote?query=query&author=author"
 
     if error:
         print(error)
         return flask.jsonify({"error": error}), 422
-    return flask.jsonify({"result": url_for('taskstatus', task_id=quote_task.id)}), 202, {"location": url_for('taskstatus', task_id=quote_task.id)}
+    return flask.jsonify({"result": url_for('taskstatus', task_id=quote_task)}), 202, {"location": url_for('taskstatus', task_id=quote_task)}
 
 @app.route('/get_result/<task_id>')
 def taskstatus(task_id):
-    task = queue_search.AsyncResult(task_id)
-    if task.state == "PENDING":
-        response = {"state": task.state}
-    elif task.state != "FAILURE":
-        response = {
-                'state': task.state}
-        response["result"] = task.result
+    task = queue_bot.request_objs[task_id]
+    if task == "PENDING":
+        response = {"state": task}
+    elif task is not None:
+        response = task
     else:
-        response = {"state": task.state}
+        response = {"success": False, "msg": "Task not found"}
     return jsonify(response)
--- queue_bot.py
+++ queue_bot.py
@@ -4,10 +4,12 @@
 A helper script that wraps up QuoteBot to run as a queue with a worker on the side.
 """
 
-from Queue import Queue
+from queue import Queue
 from threading import Thread
+import threading
 import quote_bot
 import random
+import string
 
 ID_LEN = 8
 
@@ -22,13 +24,13 @@ def run_queue(q):
         query, author, reqId = q.get()
         gr = quote_bot.GoodReads()
         result = gr.search(query, author)
-        request_obj[reqId] = result
+        request_objs[reqId] = result
         q.task_done()
 
 def add_to_queue(query, author):
-    reqId = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits for _ in range(ID_LEN)))
-    q.put(query, author, reqId)
+    reqId = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(ID_LEN))
     request_objs[reqId] = "PENDING"
+    q.put((query, author, reqId))
     return reqId
 
 def start_worker():
